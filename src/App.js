a34418b1ce9b2ec039de3bcf50ac05993c31af3a
hyomin <leehm123456@gmail.com>
2022-11-06 23:21:14 +0900

    Card reverse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes} from "react-router-dom";
+import Join from "./component/join/Join";
+import Main from "./component/Main/Main";
+import Login from "./component/login/Login";
+import SelectLogin from "./component/login/SelectLogin";
+import Play from "./component/play/Play";
+import CreateCard from "./card/CreateCard";
+import Server from "./Server";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path ="/" exact = {true} element={<Main/>} />
+        <Route path ="/login" exact = {true} element={<Login/>}/>
+        <Route path ="/join" exact = {true} element={<Join/>}/>
+        <Route path ="loginselect" exact={true} element={<SelectLogin/>}/>
+        <Route path ="/play" exact={true} element={<Play/>}/>
+        <Route path ="/server" exact={true} element={<Server/>}/>
+
+      </Routes>
   );
 }
 
